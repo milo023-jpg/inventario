e1d2ea5c4986ed3106977a24ac4309f652b0e1a0
Camilo <miloar023@gmail.com>
2025-11-16 16:54:26 -0500

    Completo Parte 1

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import cors from "cors";
 import categoriasRoutes from './routes/categorias.routes.js';
+
 
 const app = express();
 
 app.set('port',5000);
+
+app.use(cors());
 
 // Routes
 app.use(categoriasRoutes)
--- backend/src/controllers/categorias.controller.js
+++ backend/src/controllers/categorias.controller.js
@@ -3,9 +3,9 @@ import getConnection from "../db/database.js";
 const getCategorias = async (req, res) => {
     try {
         const connection = await getConnection();
-        const result = await connection.query("SELECT CategoriaID, CategoriaNombre, Descripcion, Imagen FROM categorias");
+        const [rows] = await connection.query("SELECT CategoriaID, CategoriaNombre, Descripcion, Imagen FROM categorias");
 
-        res.json(result)
+        res.json(rows)
     } catch (error) {
         console.error("Error:");
     }
